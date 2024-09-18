5d787c9bad2587518d044f93e797500596f231b1
Vanderleivj <vvj2005@hotmail.com>
2024-09-17 22:58:34 -0300

    feat: Adicionar redirecionamento para dispositivos Android e iOS

--- app/page.tsx
+++ app/page.tsx
@@ -13,15 +13,20 @@ export default function Home() {
     // Detectar dispositivo
     if (/android/i.test(userAgent)) {
       setDeviceType("Android"); // Define o tipo de dispositivo como "Android"
-      window.location.href =
-        "https://play.google.com/store/apps/details?id=br.com.popdez.passenger.drivermachine&pli=1";
+      if (window.top) {
+        window.top.location.href =
+          "https://play.google.com/store/apps/details?id=br.com.popdez.passenger.drivermachine&pli=1";
+      }
+
       // router.push(
       //   "https://play.google.com/store/apps/details?id=br.com.popdez.passenger.drivermachine&pli=1"
       // ); // Redireciona para o link A (exemplo)
     } else if (/iphone|ipad|ipod/i.test(userAgent)) {
       setDeviceType("iOS"); // Define o tipo de dispositivo como "iOS"
-      window.location.href =
-        "https://apps.apple.com/br/app/pop10-brasil/id1660040535"; // Redireciona para o link A (exemplo)
+      if (window.top) {
+        window.top.location.href =
+          "https://apps.apple.com/br/app/pop10-brasil/id1660040535";
+      }
     } else {
       setDeviceType("Desktop"); // Define o tipo de dispositivo como "Desktop"
       // router.push("/link-b"); // Redireciona para o link B (exemplo)
@@ -29,5 +34,5 @@ export default function Home() {
     console.info("Dispositivo detectado:", deviceType);
   }, [router]);
 
-  return <div />;
+  return <div>tt</div>;
 }
